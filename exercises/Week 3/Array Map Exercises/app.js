07c6c2443d343099e98008e600c37f968e794d44
Cody Jenkins <cody.m.jenkins@gmail.com>
2019-05-01 15:47:11 -0600

    update after completing Business Time project

---
+++ exercises/Week 3/Array Map Exercises/app.js
@@ -0,0 +1,8 @@
+// //1. Make an array of numbers that are doubles of the first array
+// const arr = [2, 5, 100]
+// function doubleNumbers(arr){
+     
+//   }
+  
+//   console.log(doubleNumbers())
+
